baab6fede0e4115700e31b7b2c9baf7ab6869bcc
Bokoko <kamabokoman1042@gmail.com>
2020-08-30 22:39:10 +0900

    ogp設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,15 +14,30 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'OTTgang',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content:
+          '我々OTTgang（オーティーティーギャング）は、PC一台で音楽制作をする卓です。劇伴、EDM、HipHopなど、ジャンルを問わず頭の中にある自分の世界観を音楽で表現し、それらをOTTサービス（Youtube、SoundCloudなど）を用いて世界中に公開していきます。',
       },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '~/assets/images/ott_logo.png',
+      },
+      { hid: 'og:title', property: 'og:title', content: 'OTTgang' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '我々OTTgang（オーティーティーギャング）は、PC一台で音楽制作をする卓です。劇伴、EDM、HipHopなど、ジャンルを問わず頭の中にある自分の世界観を音楽で表現し、それらをOTTサービス（Youtube、SoundCloudなど）を用いて世界中に公開していきます。',
+      },
+      { name: 'twitter:card', content: 'summary' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
