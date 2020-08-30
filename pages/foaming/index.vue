03a2f5e8ff6549eeeec41c4589034b7aafbdcec7
Bokoko <kamabokoman1042@gmail.com>
2020-08-30 23:15:43 +0900

    scリンク修正

--- pages/foaming/index.vue
+++ pages/foaming/index.vue
@@ -3,7 +3,7 @@
     <WorkDetails
       :iconSrc="'icon_nekami.jpg'"
       :movieSrc="'https://www.youtube.com/embed/7eZIbmq5Jiw'"
-      :soundCloudSrc="'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/669062363&color=%23b92c92&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&show_teaser=false'"
+      :soundCloudSrc="'669062363'"
       :prevImageSrc="'artwork_foaming.jpg'"
       :nextImageSrc="'artwork_foaming.jpg'"
       :prevLink="'/foaming'"
@@ -17,11 +17,11 @@
 </template>
 
 <script>
-import WorkDetails from '~/components/section/WorkDetails'
+import WorkDetails from '~/components/section/WorkDetails';
 
 export default {
   components: {
     WorkDetails,
   },
-}
+};
 </script>
