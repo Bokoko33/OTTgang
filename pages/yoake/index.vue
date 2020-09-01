feb5aa88801aa58c7a18412dd9f45c1ddedefb8b
Bokoko <kamabokoman1042@gmail.com>
2020-09-01 11:32:07 +0900

    youtube修正（保存し忘れ）

--- pages/yoake/index.vue
+++ pages/yoake/index.vue
@@ -2,7 +2,7 @@
   <div class="page-yoake">
     <WorkDetails
       :iconSrc="'icon_sharp.jpg'"
-      :movieSrc="'https://www.youtube.com/embed/7eZIbmq5Jiw'"
+      :movieSrc="''"
       :soundCloudSrc="'884880247'"
       :prevImageSrc="'artwork_i.jpg'"
       :nextImageSrc="'artwork_memento.jpg'"
@@ -11,9 +11,7 @@
     >
       <template v-slot:title>夜明け</template>
       <template v-slot:composer>シャープ</template>
-      <template v-slot:caption>
-        暗い空に日の光が差し込むイメージの曲です。成り行きで暗い感じの曲になってしまいましたが、盛り上がりを楽しんで貰えたら嬉しいです。
-      </template>
+      <template v-slot:caption>暗い空に日の光が差し込むイメージの曲です。成り行きで暗い感じの曲になってしまいましたが、盛り上がりを楽しんで貰えたら嬉しいです。</template>
     </WorkDetails>
   </div>
 </template>
